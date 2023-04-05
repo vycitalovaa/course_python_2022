45eece449bd55396a1f432ce45e3b8a441b46e3b
Anna Vyčítalová <vycitalova.anicka@gmail.com>
2023-04-05 19:47:07 +0200

    úpravy

--- ukol_2.py
+++ ukol_2.py
@@ -9,5 +9,11 @@ sklad = {
 kod_soucastky = input("Zadejte kód součástky:")
 if kod_soucastky in sklad:
     mnozstvi_soucastek = int(input("Zadejte množství součástek: "))
-if mnozstvi_soucastek <= sklad[kod_soucastky]:
-        print("Součástky máme!")
+else:
+    print("Součástka není skladem!")
+    if mnozstvi_soucastek > sklad[kod_soucastky]:
+        print("Součástky jsou dostupné pouze v omezeném množství kusů.")
+        sklad.pop(kod_soucastky)
+    else:   
+        print("Součástky máme")
+
