3e3f04d8569f310323e12b48a635cc1793550643
Anna Vyčítalová <vycitalova.anicka@gmail.com>
2023-05-10 16:05:42 +0200

    nevím jak mám praxovat s funkcí pujceno

--- ukol_7.py
+++ ukol_7.py
@@ -5,29 +5,25 @@ class Auto:
         self.najete_km = int(najete_km)
         self.dostupne = True
 
-    def puj_auto(self):
-        self.dostupne = False
-    
-    def __str__(self):
+    def pujc_auto(self):
+        self.dostupne = False        
         if self.dostupne:
             pujceno_zprava = "Vozidlo není k dispozici"
         else:
             pujceno_zprava = "Potvrzuji zapůjčení vozidla"
 
-    def info(self):
-        return f"Máme vozidlo {self.typ_vozidla} s registrační značkou {self.registracni_znacka}."
+    def getInfo(self):
+        return f"Vozidlo {self.typ_vozidla} s registrační značkou {self.registracni_znacka}"
 auto1 = Auto("4A2 3020", "Peugeot 403 Cabrio", 47534)
 auto2 = Auto("1P3 4747", "Škoda Octavia", 41253)
-print(auto2.info())
-auta = [auto1, auto2]
 
 pujcovane_vozidlo = input("Zadej značku vozidla, které si chceš půjčit:")
 if pujcovane_vozidlo == "Peugeot":
-    def info(self):
-        return f"Máme vozidlo {self.auto1}. {self.pujceno_zprava}"
+    print(f"Vozidlo {auto1.getInfo()}. {auto1.pujc_auto()}.")
 elif pujcovane_vozidlo == "Škoda":
-    def info(self):
-        return f"Máme vozidlo {self.auto2}. {self.pujceno_zprava}"
+    print(f"Vozidlo {auto1.getInfo()}. {auto2.pujc_auto()}.")
 else:
-    def info(self):
-        return f"Vozidlo nemáme. {self.pujceno_zprava}"
+    print(f"Vozidlo nemáme.")
+
+    
+        
