3a8f4210997bea53584657b4ecd02558f21e9460
Anna Vyčítalová <vycitalova.anicka@gmail.com>
2023-05-12 06:26:13 +0200

    nova_verze

--- ukol_7_2_verze.py
+++ ukol_7_2_verze.py
@@ -5,35 +5,42 @@ class Auto:
         self.najete_km = int(najete_km)
         self.dostupne = True
 
-    def pujc_auto(self):
-        self.dostupne = False    
+    def pujc_auto(self):        
         if self.dostupne:
-            pujceno_zprava = "Vozidlo není k dispozici"
-        else:
-            pujceno_zprava = "Potvrzuji zapůjčení vozidla"
+            self.dostupne = False 
+            print("Vozidlo je k zapůjčení")            
+        else: 
+            print("Vozidlo není dostupné")
+       
+
 
     def getInfo(self):
         return f"Máme vozidlo {self.typ_vozidla} s registrační značkou {self.registracni_znacka}."
 
 auto_1 = Auto("4A2 3020", "Peugeot 403 Cabrio", 47534)
 auto_2 = Auto("1P3 4747", "Škoda Octavia", 41253)
-print(auto2.getInfo())
+
 
 pujcovane_vozidlo = input("Zadej značku vozidla, které si chceš půjčit:")
 if pujcovane_vozidlo == "Peugeot":
-    auto_1.pujc_auto()
-    auto_1.getInfo()
+    print(auto_1.pujc_auto())
+    print(auto_1.getInfo())
+    
 elif pujcovane_vozidlo == "Škoda":
-    auto_2.pujc_auto()
-    auto_2.getInfo()
+    print(auto_2.pujc_auto())
+    print(auto_2.getInfo())
+else:
+    print(f"Půjčované vozidlo není k dipozici")
+pujcovane_vozidlo = input("Zadej značku vozidla, které si chceš půjčit:")
+if pujcovane_vozidlo == "Peugeot":
+    print(auto_1.pujc_auto())
+    print(auto_1.getInfo())
+    
+elif pujcovane_vozidlo == "Škoda":
+    print(auto_2.pujc_auto())
+    print(auto_2.getInfo())
 else:
     print(f"Půjčované vozidlo není k dipozici")
 
-if pujcovane_vozidlo == "Peugeot":
-    auto_1.pujc_auto()
-    auto_1.getInfo()
-elif pujcovane_vozidlo == "Škoda":
-    auto_2.pujc_auto()
-    auto_2.getInfo()
-else:
-    print(f"Půjčované vozidlo není k dipozici")
+
+
